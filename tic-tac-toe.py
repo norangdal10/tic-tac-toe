be163dee038d832d56a9bbec046bb6190c98da05
root <norangdal10@naver.com>
2017-11-26 14:59:04 +0900

    tic-tac

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -8,13 +8,32 @@ def checked(i) :
             return
       button["text"] = player 
       button["bg"] = "yellow"
-
+      CheckWinner(player)
+      
       if player == "X" :
             player = "O"
-            button["bg"] = "yellow"
+            button["bg"] = "yellow" 
       else :
             player = "X"
             button["bg"] = "lightgreen"
+
+def Win(player):
+      global Msg
+      Msg = Message(window, text = " " + player + " win!")
+      Msg.grid(row = 4, column = 1)
+      
+      
+def CheckWinner(player):
+      for j in range(3):
+            if list[j]["text"] == list[j+3]["text"] == list[j+6]["text"] == player :
+                  Win(player)
+            if list[j*3]["text"] == list[j*3+1]["text"] == list[j*3+2]["text"] == player :
+                  Win(player)
+      if list[0]["text"] == list[4]["text"] == list[8]["text"] == player :
+            Win(player)
+      if list[2]["text"] == list[4]["text"] == list[6]["text"] == player :
+            Win(player)
+
 
 window = Tk()
 player = "X"
@@ -24,6 +43,7 @@ for i in range(9) :
       b = Button(window, text="     ", command=lambda k=i: checked(k))
       b.grid(row=i//3, column=i%3)
       list.append(b)
+
 
 window.mainloop()
 
